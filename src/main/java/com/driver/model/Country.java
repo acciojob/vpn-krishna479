21e67faec19341ea1662ec8b609ced2286ec6336
krishna479 <krishankant.sharma@gmail.com>
2023-03-04 18:12:17 +0530

    third commit

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -1,1 +1,67 @@
-// Note: Do not write @Enumerated annotation above CountryName in this model.
+package com.driver.model;// Note: Do not write @Enumerated annotation above CountryName in this model.
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    String code;
+
+    @Enumerated(EnumType.STRING)
+    CountryName countryName;
+
+    @ManyToOne
+    @JoinColumn
+    private ServiceProvider serviceProvider;
+
+    @OneToOne
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Country() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public CountryName getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(CountryName countryName) {
+        this.countryName = countryName;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+
+}
--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,18 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -20,13 +27,71 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin();
+        admin.setUsername(username);
+        admin.setPassword(password);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setName(providerName);
+       Admin admin =  adminRepository1.findById(adminId).get();
+       serviceProvider.setAdmin(admin);
+
+       admin.getServiceProviderList().add(serviceProvider);
+
+        adminRepository1.save(admin);
+        return admin;
+
+
+
+
+
     }
 
     @Override
-    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception {
+
+        if (countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("AUS") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("CHI")) {
+
+
+            Country country = new Country();
+
+            if (countryName.equalsIgnoreCase("IND")) {
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
+            }
+            if (countryName.equalsIgnoreCase("AUS")) {
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            if (countryName.equalsIgnoreCase("JPN")) {
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+            if (countryName.equalsIgnoreCase("CHI")) {
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            if (countryName.equalsIgnoreCase("USA")) {
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+            country.setServiceProvider(serviceProvider);
+            serviceProvider.getCountryList().add(country);
+
+            serviceProviderRepository1.save(serviceProvider);
+            return serviceProvider;
+        }
+        else{
+            throw new Exception("cannot add country");
+        }
+
+
     }
 }
